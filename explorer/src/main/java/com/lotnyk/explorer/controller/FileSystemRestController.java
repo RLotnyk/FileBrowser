79533fd8a348d2df500fb4f18a32329fcb52ae9a
RomanLotnyk <lotnik_ra@km-union.ru>
2021-07-10 20:12:49 +0300

    update controller

--- explorer/src/main/java/com/lotnyk/explorer/controller/FileSystemRestController.java
+++ explorer/src/main/java/com/lotnyk/explorer/controller/FileSystemRestController.java
@@ -1,8 +1,8 @@
 package com.lotnyk.explorer.controller;
 
 import com.lotnyk.explorer.model.Node;
-import com.lotnyk.explorer.utils.FileSystem;
-import com.lotnyk.explorer.utils.FileSystemAction;
+import com.lotnyk.explorer.utils.FileSystemStorage;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -12,41 +12,56 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-
+@Slf4j
 @RestController
 public class FileSystemRestController {
 
-    private final FileSystem system;
-    private final FileSystemAction action;
+    private final FileSystemStorage storage;
     private final List<Node> list;
 
     @Autowired
-    public FileSystemRestController(final FileSystem system, final FileSystemAction action) {
-        this.system = system;
-        this.action = action;
-        this.list = this.system.init();
+    public FileSystemRestController(FileSystemStorage storage) throws IOException {
+        this.storage = storage;
+        this.list = this.storage.createTree();
     }
 
     @GetMapping(value = "/tree", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public List<Node> tree() {
+    public List<Node> tree() throws IOException {
         return list;
     }
 
     @PostMapping(value = "/create")
-    public void create(@RequestParam String id, @RequestParam String name) {
-        action.create(id, name);
+    public void create(@RequestBody Node node) throws Exception {
+        File file = new File(String.format("%s%s%s", node.getId(), File.separator, node.getText()));
+        System.out.println(file);
+        if (storage.add(file)) {
+            log.debug(String.format("Created File: %s", file.getName()));
+        }
     }
 
     @PostMapping(value = "/delete")
-    public void delete(@RequestParam String id) throws IOException {
-        action.delete(new File(id));
+    public void delete(@RequestBody Node node) throws Exception {
+        File file = new File(node.getId());
+        storage.delete(file);
+        log.info(String.format("Deleted File: %s", file));
     }
 
     @PostMapping("/upload")
-    public void uploadFile(@RequestParam("path") String path, @RequestParam("file")MultipartFile file) throws IOException {
+    public void uploadFile(@RequestParam("path") String path, @RequestParam("file") MultipartFile file) throws Exception {
         File uploadFile = new File(path + File.separator + file.getOriginalFilename());
-        System.out.println(uploadFile.getName());
         file.transferTo(uploadFile);
-        action.uploadFile(uploadFile);
+        System.out.println(uploadFile);
+        storage.uploadFile(uploadFile);
+    }
+
+    @GetMapping("/search")
+    public List<Node> findNode(@RequestParam("str") String text) throws IOException {
+        return storage.findFile(text);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public String handle(IllegalArgumentException e) {
+        log.error(e.getMessage());
+        return e.getMessage();
     }
 }
