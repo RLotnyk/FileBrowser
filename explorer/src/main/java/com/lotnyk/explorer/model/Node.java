bb4df66ef720cb609266b531d635f2e13ae1eb60
RomanLotnyk <lotnik_ra@km-union.ru>
2021-07-10 20:13:06 +0300

    update

--- explorer/src/main/java/com/lotnyk/explorer/model/Node.java
+++ explorer/src/main/java/com/lotnyk/explorer/model/Node.java
@@ -1,9 +1,16 @@
 package com.lotnyk.explorer.model;
 
-import java.util.Objects;
+import lombok.*;
 
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@AllArgsConstructor
 public class Node {
 
+    @NonNull
+    @EqualsAndHashCode.Include
     private String id;
 
     private String parent;
@@ -12,64 +19,4 @@ public class Node {
 
     private String type;
 
-    public Node(String id, String parent, String text, String type) {
-        this.id = id;
-        this.parent = parent;
-        this.text = text;
-        this.type = type;
-    }
-
-    public Node(String id) {
-        this.id = id;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getParent() {
-        return parent;
-    }
-
-    public void setParent(String parent) {
-        this.parent = parent;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Node node = (Node) o;
-        return Objects.equals(id, node.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    @Override
-    public String toString() {
-        return String.format("%s", this.id);
-    }
 }
